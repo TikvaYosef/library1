2726218f557d804932662b3b01f8391b2cc8ab5c
TikvaYosef <tikvayosef13@gmail.com>
2021-12-22 20:47:13 +0200

    Add project files.

---
+++ Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace library1.Models
+{
+    public class Book
+    {
+        public string name;
+        public string author;
+        public int year;
+        public int numOfPages;
+
+        public Book(string name, string author, int year, int numOfPages)
+        {
+            this.name = name;
+            this.author = author;
+            this.year = year;
+            this.numOfPages = numOfPages;
+
+        }
+    }
+}
